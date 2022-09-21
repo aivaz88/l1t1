5ca1ca045d248cd5b94c30dbc73369f074fdcd13
aivaz88 <aivaz1988@mail.ru>
2022-09-21 23:51:51 +0500

    comment

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2){
+    Console.Write("max=");
+    Console.WriteLine(num1);
+    Console.Write("min=");
+    Console.WriteLine(num2);
+}
+else {
+    Console.Write("max=");
+    Console.WriteLine(num2);
+    Console.Write("min=");
+    Console.WriteLine(num1);
+}
